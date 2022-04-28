d76abe865a317d9470a3effb4623f94607da9548
amirul-afif <amirulafif.edu@gmail.com>
2022-04-29 02:16:13 +0800

    add

--- main.py
+++ main.py
@@ -25,6 +25,8 @@ item2 = Item("Laptop", 1000, 3)
 item3 = Item("Cable", 10, 5)
 item4 = Item("Mouse", 50, 5)
 item5 = Item("Keyboard", 75, 5)
+item5.apply_discount()
+print(item5.price)
 
 # print(Item.__dict__)  # All attributes for Class level
 # print(item1.__dict__)  # All attributes for Instance level
